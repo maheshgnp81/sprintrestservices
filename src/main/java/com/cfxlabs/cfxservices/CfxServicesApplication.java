1aeadaf19db0e28a36b9bb4dd22fcda4d20e7235
maheshg <maheshgnp81@gmail.com>
2022-08-07 15:54:49 +0530

    added new rest services project

---
+++ src/main/java/com/cfxlabs/cfxservices/CfxServicesApplication.java
@@ -0,0 +1,13 @@
+package com.cfxlabs.cfxservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfxServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfxServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cfxlabs/cfxservices/CfxservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cfxlabs.cfxservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CfxservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
